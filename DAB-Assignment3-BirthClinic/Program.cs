935f31eb1450aa580a77d69eacddc62a39b4bec7
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-04-28 12:01:24 +0200

    Add project files.

---
+++ DAB-Assignment3-BirthClinic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DAB_Assignment3_BirthClinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new MongoClient(
+                "mongodb://localhost:27017"
+            );
+            var database = client.GetDatabase("BirthClinic");
+            var collection = database.GetCollection<BsonDocument>("Clinicians");
+
+            var doc = new BsonDocument()
+            {
+                {"Name", "Hugo"}
+            };
+            collection.InsertOne(doc);
+
+            var dblist = client.ListDatabases().ToList();
+            foreach (var db in dblist)
+            {
+                Console.WriteLine(db);
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
